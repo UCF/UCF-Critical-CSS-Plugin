0cee6cbbeb4f76d7d1d26b2393cc8b446ec1dd86
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-04-14 12:20:46 -0400

    Changed hook we use to register the options page so post_types are registered by the time it is called

--- ucf-critical-css.php
+++ ucf-critical-css.php
@@ -23,7 +23,7 @@ namespace UCF\Critical_CSS {
 	include_once UCF_CRITICAL_CSS__PLUGIN_DIR . 'admin/config.php';
 
 	function plugin_init() {
-		add_action( 'acf/init', array( 'UCF\Critical_CSS\Admin\Config', 'add_options_page' ) );
+		add_action( 'init', array( 'UCF\Critical_CSS\Admin\Config', 'add_options_page' ), 20, 0 );
 	}
 
 	add_action( 'plugins_loaded', __NAMESPACE__ . '\plugin_init' );
